1ae2c7d0ff273f7287f77e2641e79090e3053f88
Alex Mico <alexmico2006@gmail.com>
2026-02-12 17:08:38 +0100

    feat(testimonials): redesign layout spacing and replace fake avatars with anonymous project references

--- src/componets/testimonials/Testimonials.jsx
+++ src/componets/testimonials/Testimonials.jsx
@@ -2,10 +2,15 @@ import React from 'react'
 import { useTranslation } from 'react-i18next'
 import './testimonials.css'
 import { FaQuoteLeft } from 'react-icons/fa'
+<<<<<<< HEAD
 import { Autoplay, Navigation } from 'swiper/modules'
+=======
+import { Autoplay, Navigation, Pagination } from 'swiper/modules'
+>>>>>>> 9f05531 (feat(testimonials): redesign layout spacing and replace fake avatars with anonymous project references)
 import { Swiper, SwiperSlide } from 'swiper/react'
 import 'swiper/css'
 import 'swiper/css/navigation'
+import 'swiper/css/pagination'
 
 const Testimonials = () => {
   const { t } = useTranslation();
@@ -18,26 +23,31 @@ const Testimonials = () => {
 
       <div className="container testimonials__container">
         <Swiper
-          modules={[Navigation, Autoplay]}
+          className="testimonials__slider"
+          modules={[Navigation, Pagination, Autoplay]}
           spaceBetween={40}
           slidesPerView={1}
           navigation
+          pagination={{ clickable: true }}
           autoplay={{ delay: 5000, disableOnInteraction: false }}
           loop={true}
         >
           {testimonials.map((testimonial, index) => (
             <SwiperSlide key={index}>
               <div className="testimonial">
-                <div className="client__avatar">
-                  <img src={`https://randomuser.me/api/portraits/${index % 2 === 0 ? 'men' : 'women'}/${32 + index * 12}.jpg`} alt={testimonial.name} />
+                <div className="testimonial__header">
+                  <span className="client__badge">{t('testimonials.anonymous_badge')}</span>
                 </div>
-                <div className="testimonial__content">
-                  <FaQuoteLeft className="testimonial__icon" />
-                  <p className="client__review">{testimonial.review}</p>
+                <div className="testimonial__body">
+                  <div className="testimonial__content">
+                    <FaQuoteLeft className="testimonial__icon" />
+                    <p className="client__review">{testimonial.review}</p>
+                  </div>
                 </div>
-                <div className="client__info">
+                <div className="testimonial__footer client__info">
                   <h5 className="client__name">{testimonial.name}</h5>
                   <small className="client__role">{testimonial.role}</small>
+                  <small className="client__context">{testimonial.context}</small>
                 </div>
               </div>
             </SwiperSlide>
